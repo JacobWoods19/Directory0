59f93532b964ad2144fc4e084cb4d5e0f9e3ca4e
Jacob Woods <68571928+jacobwoods45@users.noreply.github.com>
2023-03-23 16:45:27 -0500

    first commit

--- client/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = "https://qqfktkpxbhnruikwfzbp.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxZmt0a3B4YmhucnVpa3dmemJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcxODE3NzcsImV4cCI6MTk5Mjc1Nzc3N30.AKbmKpXSNpEfhQbkNypsVHbo273wKUN4wsXY02VTbkA"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
