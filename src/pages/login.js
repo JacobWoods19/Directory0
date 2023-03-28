c39c9b339ff9fb777d8cb6be3b48a0a5496444a1
Jacob Woods <68571928+jacobwoods45@users.noreply.github.com>
2023-03-28 11:52:49 -0500

    03/28/23

--- src/pages/login.js
+++ src/pages/login.js
@@ -51,7 +51,6 @@ export default function Auth() {
                 onClick={(e) => {
                   e.preventDefault()
                   handleLogin(email)
-                  emailSent()
                 }}
                 className="my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 disabled={loading}
